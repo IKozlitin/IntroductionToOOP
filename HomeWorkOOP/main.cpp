b4be213afc8553f97b729a7196a6248793309f1a
Игорь <@LAPTOP-NLBVKIFJ>
2022-11-07 13:44:25 +0300

    Add project files.

---
+++ HomeWorkOOP/main.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()
+	{
+		return x;
+	}
+	double get_y()
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+};
+
+//#define STRUCT_POINT
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+
+#ifdef STRUCT_POINT
+	int a;
+	Point A;
+
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUCT_POINT
+}
---
+++ InproductionToOOP/main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+};
+
+//#define STRUCT_POINT
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	
+#ifdef STRUCT_POINT
+	cout << "Hello OOP" << endl;
+
+	int a; //Объявление переменной 'a' типа 'int'
+	Point A; //Объявление переменной 'A' типа 'Point'
+			//Создание объекта 'A' структуры 'Point'
+			//Объявление экземпляра 'A' структуры 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif //STRUCT_POINT
+
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+}

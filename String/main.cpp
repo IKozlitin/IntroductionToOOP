12528b00194295e038e963178803eb6f926efe15
Игорь <@LAPTOP-NLBVKIFJ>
2022-12-13 17:54:50 +0300

    string

--- String/main.cpp
+++ String/main.cpp
@@ -9,7 +9,18 @@ class String
 						//size_t - unsigned int
 	char* str;			//адрес строки в динамической памяти
 public:
-
+	const char* get_str()const
+	{
+		return str;
+	}
+	char* get_str()
+	{
+		return str;
+	}
+	size_t get_size()const
+	{
+		return size;
+	}
 	//						Constructors:
 	explicit String(size_t size = 80)
 	{
@@ -38,7 +49,7 @@ public:
 		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout.width(WIDTH);
 		cout << left << "Copy constructor: " << this << endl;
-	}
+	}	
 	~String()
 	{
 		delete[] this->str;
@@ -56,15 +67,21 @@ public:
 
 };
 
-String operator+()
+String operator+(const String& left, const String& right)
 {
-
+	String str(left.get_size() + right.get_size() - 1);
+	for (int i = 0; i < left.get_size(); i++)
+		str.get_str()[i] = left.get_str()[i];
+	for (int i = 0; i < right.get_size(); i++)
+		str.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+	return str;
 }
 
-ostream& operator<<()
+ostream& operator<<(ostream& os, const String& obj)
 {
-
+	return os << obj.get_str();
 }
+
 void main()
 {
 	setlocale(LC_ALL, "Rus");
@@ -81,5 +98,6 @@ void main()
 	str2.print();
 
 	String str3 = str1 + str2;
+	//str3.print();
 	cout << str3 << endl;
 }
